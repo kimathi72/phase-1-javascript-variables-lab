ccebc3405c8a8e1f9c8bd4a6bdb26d0f03e76dbd
roy <kimathiwaweru@gmail.com>
2023-03-15 09:56:24 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // Code your solution in this file!
+
+
+function beforeEach () {
+ const companyName = undefined;
+ let mostProfitableNeighborhood = undefined;
+ let companyCeo = undefined;
+}
+beforeEach ();
+//variable company declare using const
 const companyName = "Scuber";
+
+// variable mostProfitableNeighborhood declared using let
 let mostProfitableNeighborhood = "Chelsea";
+
+//variable companyCeo declared using let
 let companyCeo = "Susan Smith";
